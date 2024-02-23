df0be7f553844e2e39faba495f5b4a220e4141ae
zhouhui <chowhwei@gmail.com>
2024-02-23 16:22:08 +0800

    优化，如果过去5分钟内日志太多，只取不超过10万条，否则取上次到现在的日志数量

--- bash/get_log_time.sh
+++ bash/get_log_time.sh
@@ -2,6 +2,31 @@
 
 file="last_log_point"
 
+sto_unixtime()
+{
+  date -d "$(echo "$1" | sed -e 's,/,-,g' -e 's,:, ,')" +"%s"
+}
+
+get_time_from_log()
+{
+  # 23/Feb/2024:10:54:27
+  now=$(tail -1 /var/log/nginx/access.log | grep -ioE "[0-9]{2}\/[a-z]{3}\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2}")
+  now_ts=$(sto_unixtime "$now")
+  now_nf=$(date -d @"$now_ts" "+%Y-%m-%d %H:%M:%S")
+  #23/Feb/2024:10:51:38
+     #1708656698
+     #2024-02-23 10:51:38
+     #2024-02-23 10:51:38
+  echo "$now"
+  echo "$now_ts"
+  echo "$now_nf"
+  date -d "$(echo "$now" | sed -e 's,/,-,g' -e 's,:, ,')" +"%Y-%m-%d %H:%M:%S"
+}
+
+get_time_from_log
+exit 0
+
+
 get_log_time()
 {
   file=$1
@@ -24,11 +49,6 @@ set_log_time()
 #  tail -1 "$1" |
 #}
 
-sto_unixtime()
-{
-  date -d "$(echo "$1" | sed -e 's,/,-,g' -e 's,:, ,')" +"%s"
-}
-
 # shellcheck disable=SC2046
 last=$(get_log_time "$file")
 
@@ -40,17 +60,17 @@ time_5=`date -d "5 minutes ago" "+%d/%b/%Y:%H:%M:%S"`
 time=`date "+%d/%b/%Y:%H:%M:%S"`
 
 set_log_time "$file" "$time"
-
-if [ $(sto_unixtime "$last") -ge $(sto_unixtime "$time_5") ]; then
-  start=$last
-else
-  start=$time_5
-fi
+#
+#if [ $(sto_unixtime "$last") -ge $(sto_unixtime "$time_5") ]; then
+#  start=$last
+#else
+#  start=$time_5
+#fi
 
 
 echo "$last"
 echo "$time_5"
-echo "$start"
+#echo "$start"
 echo "$time"
 
 sto_unixtime "$last"
